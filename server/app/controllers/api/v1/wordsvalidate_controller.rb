5a232ff45234003dbcc911b0b8592b4901065263
sandeep rajbhandari <sandeepawaz@gmail.com>
2020-01-06 20:17:55 +0545

    update

--- server/app/controllers/api/v1/wordsvalidate_controller.rb
+++ server/app/controllers/api/v1/wordsvalidate_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::WordsvalidateController < ApplicationController
   def index
-    word = item_params[:word].strip
-    is_valid = V1::WordsService.instance.find_word(word)
-    count = is_valid ? V1::WordsService.instance.calculate_points(word) : 0
-    json_response(isValid: is_valid, points: count)
+    begin
+      word = item_params[:word].strip
+      is_valid = V1::WordsService.instance.find_word(word)
+      count = is_valid ? V1::WordsService.instance.calculate_points(word) : 0
+      json_response(isValid: is_valid, points: count)
+    rescue StandardError => e
+      logger.error(e);
+      json_response(error:e)
+    end
   end
 
   private
--- server/config/environment.rb
+++ server/config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.logger = Logger.new(STDOUT)
+Rails.logger.level = Logger::DEBUG
+Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
